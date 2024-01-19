b360567cf27693a290d9ddc56de2cc26ae699f57
Chainz <61576690+Chainz7@users.noreply.github.com>
2024-01-19 10:47:16 +0700

    Services & Testimonials Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,8 +40,8 @@
                             <span>HOTEL OPERATOR</span>
                         </div>
                         <div class="body-left-main">
-                            <span class="text-header">Membantu Menjalankan <br> Operasi Bisnis Pariwisata Anda</span>
-                            <span class="text-description">GWA membantu mengoperasikan keseluruhan layanan hotel, <br> menjadikan mitra lebih percaya diri dalam menjalankan bisnis.</span>
+                            <span class="text-header-hero">Membantu Menjalankan <br> Operasi Bisnis Pariwisata Anda</span>
+                            <span class="text-description-hero">GWA membantu mengoperasikan keseluruhan layanan hotel, <br> menjadikan mitra lebih percaya diri dalam menjalankan bisnis.</span>
                         </div>
                         <div class="body-left-button">
                             <span>Konsultasikan Bisnis Saya</span>
@@ -64,7 +64,7 @@
                 </div>
                 <div class="workflow-pathway">
                     <div class="workflow-1">
-                        <div class="pathway-1" style="margin-top: 15rem;">
+                        <div class="pathway-1" style="margin-top: 18rem;">
                             <div class="pathway-circle">
                                 <img src="{{URL('/images/Achievement-1.png')}}" alt="">
                             </div>
@@ -73,7 +73,7 @@
                         </div>
                     </div>
                     <div class="workflow-1">
-                        <div class="pathway-1" style="margin-left: 5rem; margin-top: 9rem; width: 40rem">
+                        <div class="pathway-1" style="margin-left: 5rem; margin-top: 12rem; width: 40rem">
                             <div class="pathway-circle">
                                 <img src="{{URL('/images/Achievement-2.png')}}" alt="">
                             </div>
@@ -82,7 +82,7 @@
                         </div>
                     </div>
                     <div class="workflow-1">
-                        <div class="pathway-1" style="margin-left: 1rem; margin-top: 11rem; width: 43rem">
+                        <div class="pathway-1" style="margin-left: 1rem; margin-top: 14rem; width: 43rem">
                             <div style="display: flex">
                                 <div class="pathway-circle blue">
                                     <img src="{{URL('/images/Achievement-3.png')}}" alt="">
@@ -94,7 +94,7 @@
                         </div>
                     </div>
                     <div class="workflow-1">
-                        <div class="pathway-1" style="margin-left: -12rem; margin-top: -2rem; width: 37rem">
+                        <div class="pathway-1" style="margin-left: -12rem; margin-top: 1rem; width: 37rem">
                             <div class="pathway-circle">
                                 <img src="{{URL('/images/Achievement-4.png')}}" alt="">
                             </div>
@@ -105,8 +105,126 @@
                 </div>
             </div>
             <div class="services container">
-                asd
+                <img class="services-bg" src="{{URL('/images/BG-Line-3.png')}}" alt="">
+                <div class="highlight">
+                    <img src="{{URL('/images/Line.png')}}" alt="">
+                    <span>OUR SERVICES</span>
+                </div>
+                <div class="text-header">
+                    Apa Saja yang Bisa Kami Bantu ?
+                </div>
+                <div class="services-boxs">
+                    <div class="services-box">
+                        <div class="services-box-top">
+                            <img src="{{URL('/images/Services-1.png')}}" alt="">
+                            <span>01</span>
+                        </div>
+                        <span class="services-box-title">
+                            Revenue <br> Management Service
+                        </span>
+                        <button class="services-box-button">
+                            <span>Saya Tertarik</span>
+                            <img src="{{URL('/images/Arrow-Right.png')}}" alt="">
+                        </button>
+                    </div>
+                    <div class="services-box">
+                        <div class="services-box-top">
+                            <img src="{{URL('/images/Services-2.png')}}" alt="">
+                            <span>02</span>
+                        </div>
+                        <span class="services-box-title">
+                            Full Manage <br> Service
+                        </span>
+                        <button class="services-box-button">
+                            <span>Saya Tertarik</span>
+                            <img src="{{URL('/images/Arrow-Right.png')}}" alt="">
+                        </button>
+                    </div>
+                    <div class="services-box">
+                        <div class="services-box-top">
+                            <img src="{{URL('/images/Services-3.png')}}" alt="">
+                            <span>03</span>
+                        </div>
+                        <span class="services-box-title">
+                            Asset Monetize <br> Service
+                        </span>
+                        <button class="services-box-button">
+                            <span>Saya Tertarik</span>
+                            <img src="{{URL('/images/Arrow-Right.png')}}" alt="">
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="projects container">
+                <div class="highlight-projects">
+                    <img src="{{URL('/images/Line.png')}}" alt="">
+                    <span>OUR SERVICES</span>
+                    <img src="{{URL('/images/Line.png')}}" alt="">
+                </div>
+                <div class="text-header-projects">
+                    Project Terbaru Kami
+                </div>
+                <div class="projects-boxs">
+                    <img class="projects-main-image" src="{{ URL('/images/Image-2.png') }}" alt="">
+                    <div class="projects-box">
+                        <div class="box-previous">
+                            <img src="{{ URL('/images/Left.png') }}" alt="">
+                        </div>
+                        <div class="box-main">
+                            <div class="projects-main-left">
+                                <span class="projects-box-subtitle">1/4 OPERATIONAL PROJECT</span>
+                                <span class="projects-box-title">Townhouse Oak</span>
+                                <span class="projects-box-description">Brand tertinggi di OYO</span>
+                            </div>
+                            <div class="projects-main-right">
+                                <div class="projects-box-right">
+                                    <img src="{{ URL('/images/Right-White.png') }}" alt="">
+                                </div>
+                                <span class="projects-box-subdescription">Lihat Detail</span>
+                            </div>
+                        </div>
+                        <div class="box-next">
+                            <img src="{{ URL('/images/Right.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonials container">
+                <div class="testimonials-left">
+                    <div class="highlight">
+                        <img src="{{URL('/images/Line.png')}}" alt="">
+                        <span>OUR TESTIMONIALS</span>
+                    </div>
+                    <div class="text-header">
+                        Yang Klien Kami Katakan
+                    </div>
+                    <div class="testimonials-description-box">
+                        <img src="{{ URL('/images/Quotes-Sign.png') }}" alt="">
+                        <span class="testimonials-description">GWA Group membantu hotel
+                            saya untuk menjangkau lebih banyak client dan memberikan saran monetisasi yang tidak pernah saya bayangkan sebelumnya
+                        </span>
+                    </div>
+                    <div class="testimonials-clients">
+                        <img src="{{ URL('/images/testimonial-1.png') }}" alt="">
+                        <div class="testimonials-client">
+                            <span class="testimonials-client-title">Chaim Desmond</span>
+                            <span class="testimonials-client-subtitle">CEO of Yellow Hotel</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="testimonials-right">
+                    <img class="testimonials-image" src="{{ URL('/images/Image-3.png') }}" alt="">
+                    <div class="tesimonials-button">
+                        <div class="testimonials-button-left">
+                            <img src="{{ URL('/images/Left-White.png') }}" alt="">
+                        </div>
+                        <div class="testimonials-button-right">
+                            <img src="{{ URL('/images/Right-Arrow2.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
     </body>
 </html>
